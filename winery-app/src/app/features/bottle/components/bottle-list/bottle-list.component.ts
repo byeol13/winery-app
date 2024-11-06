4822dd37a9506d39ca03a273e0f1203023fdf4e0
ranjahasrama <ranjahasrama@gmail.com>
2024-11-06 15:31:25 +0100

    created bottle-list component

---
+++ winery-app/src/app/features/bottle/components/bottle-list/bottle-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bottle-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './bottle-list.component.html',
+  styleUrl: './bottle-list.component.css'
+})
+export class BottleListComponent {
+
+}
