2c243fc849957b61327f419888faf5d71198cc29
ranjahasrama <ranjahasrama@gmail.com>
2024-11-06 20:11:50 +0100

    8696hmcfx Create store-details component

---
+++ winery-app/src/app/features/store/components/store-details/store-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-store-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './store-details.component.html',
+  styleUrl: './store-details.component.css'
+})
+export class StoreDetailsComponent {
+
+}
