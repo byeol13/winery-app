e66b77bb761c4ec24fa41a71a85beaded4080f79
ranjahasrama <ranjahasrama@gmail.com>
2024-11-06 20:09:29 +0100

    8696hrdd3 Create region-details component

---
+++ winery-app/src/app/features/region/components/region-details/region-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-region-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './region-details.component.html',
+  styleUrl: './region-details.component.css'
+})
+export class RegionDetailsComponent {
+
+}
