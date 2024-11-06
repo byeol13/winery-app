20a9e921f754302b99e52ffd99ed4c850ebec33f
ranjahasrama <ranjahasrama@gmail.com>
2024-11-06 15:33:19 +0100

    created bottle-details component

---
+++ winery-app/src/app/features/bottle/components/bottle-details/bottle-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bottle-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './bottle-details.component.html',
+  styleUrl: './bottle-details.component.css'
+})
+export class BottleDetailsComponent {
+
+}
