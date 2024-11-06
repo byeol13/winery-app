caea3506acbc82270d087bbfab515333f4346f42
ranjahasrama <ranjahasrama@gmail.com>
2024-11-06 19:36:31 +0100

    8696hmypm Create customer-order-details component

---
+++ winery-app/src/app/features/customerOrder/components/customer-order-details/customer-order-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-order-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './customer-order-details.component.html',
+  styleUrl: './customer-order-details.component.css'
+})
+export class CustomerOrderDetailsComponent {
+
+}
