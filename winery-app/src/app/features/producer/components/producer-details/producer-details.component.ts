41a6c8fc1e97265586ed28fb2e248e14c1d3ddf1
ranjahasrama <ranjahasrama@gmail.com>
2024-11-06 18:59:48 +0100

    8696hkd2p - Create producer-details component

---
+++ winery-app/src/app/features/producer/components/producer-details/producer-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-producer-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './producer-details.component.html',
+  styleUrl: './producer-details.component.css'
+})
+export class ProducerDetailsComponent {
+
+}
