155dda654fd8d6b2bd16669856804d1944c3df15
ranjahasrama <ranjahasrama@gmail.com>
2024-11-06 15:37:45 +0100

    created producer-list component

---
+++ winery-app/src/app/features/producer/components/producer-list/producer-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-producer-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './producer-list.component.html',
+  styleUrl: './producer-list.component.css'
+})
+export class ProducerListComponent {
+
+}
