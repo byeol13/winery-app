af291831afd5a000bbab226ffed366f3a62966b2
ranjahasrama <ranjahasrama@gmail.com>
2024-11-06 19:33:21 +0100

    8696hmyc1- Create customer-order-list component

---
+++ winery-app/src/app/features/customerOrder/components/customer-order-list/customer-order-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-order-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './customer-order-list.component.html',
+  styleUrl: './customer-order-list.component.css'
+})
+export class CustomerOrderListComponent {
+
+}
