e7564c65585852c218cb336a21f9f4377ca583aa
ranjahasrama <ranjahasrama@gmail.com>
2024-11-06 19:59:59 +0100

    8696hrerg Create invoice-item-details component

---
+++ winery-app/src/app/features/invoiceItem/components/invoice-item-details/invoice-item-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-invoice-item-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './invoice-item-details.component.html',
+  styleUrl: './invoice-item-details.component.css'
+})
+export class InvoiceItemDetailsComponent {
+
+}
