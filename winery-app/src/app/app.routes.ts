260b68d177832bd667bff882ca3dcd2313246f49
ranjahasrama <ranjahasrama@gmail.com>
2024-11-08 20:25:32 +0100

    Shfaq menu ne dashboard component

--- winery-app/src/app/app.routes.ts
+++ winery-app/src/app/app.routes.ts
@@ -1,3 +1,38 @@
 import { Routes } from '@angular/router';
+import { BottleListComponent } from './features/bottle/components/bottle-list/bottle-list.component';
+import { CategoryListComponent } from './features/category/components/category-list/category-list.component';
+import { CityListComponent } from './features/city/components/city-list/city-list.component';
+import { CountryListComponent } from './features/country/components/country-list/country-list.component';
+import { CustomerListComponent } from './features/customer/components/customer-list/customer-list.component';
+import { CustomerOrderListComponent } from './features/customerOrder/components/customer-order-list/customer-order-list.component';
+import { CustomerOrderItemListComponent } from './features/customerOrderItem/components/customer-order-item-list/customer-order-item-list.component';
+import { EmployeeListComponent } from './features/employee/components/employee-list/employee-list.component';
+import { InventoryListComponent } from './features/inventory/components/inventory-list/inventory-list.component';
+import { InvoiceListComponent } from './features/invoice/components/invoice-list/invoice-list.component';
+import { OrderListComponent } from './features/order/components/order-list/order-list.component';
+import { OrderItemListComponent } from './features/orderItem/components/order-item-list/order-item-list.component';
+import { InvoiceItemListComponent } from './features/invoiceItem/components/invoice-item-list/invoice-item-list.component';
+import { ProducerListComponent } from './features/producer/components/producer-list/producer-list.component';
+import { RegionListComponent } from './features/region/components/region-list/region-list.component';
+import { StoreListComponent } from './features/store/components/store-list/store-list.component';
+import { SupplierListComponent } from './features/supplier/components/supplier-list/supplier-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'dashboard/bottle', component: BottleListComponent},
+  {path: 'dashboard/category', component: CategoryListComponent},
+  {path: 'dashboard/city', component: CityListComponent},
+  {path: 'dashboard/country', component: CountryListComponent},
+  {path: 'dashboard/customer', component: CustomerListComponent},
+  {path: 'dashboard/customerOrder', component: CustomerOrderListComponent},
+  {path: 'dashboard/customerOrderItem', component: CustomerOrderItemListComponent},
+  {path: 'dashboard/employee', component: EmployeeListComponent},
+  {path: 'dashboard/inventory', component: InventoryListComponent},
+  {path: 'dashboard/invoice', component: InvoiceListComponent},
+  {path: 'dashboard/invoiceItem', component: InvoiceItemListComponent},
+  {path: 'dashboard/order', component: OrderListComponent},
+  {path: 'dashboard/orderItem', component: OrderItemListComponent},
+  {path: 'dashboard/producer', component: ProducerListComponent},
+  {path: 'dashboard/region', component: RegionListComponent},
+  {path: 'dashboard/store', component: StoreListComponent},
+  {path: 'dashboard/supplier', component: SupplierListComponent}
+];
--- winery-app/src/app/dashboard/dashboard.component.ts
+++ winery-app/src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,41 @@
+import { routes } from './../app.routes';
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule, MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule, MatButtonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
 
+  routes: {path: string, label: string}[] = [
+    {path: '/dashboard/bottle', label: 'Bottle'},
+    {path: '/dashboard/category', label: 'Category'},
+    {path: '/dashboard/city', label: 'City'},
+    {path: '/dashboard/country', label: 'Country'},
+    {path: '/dashboard/customer', label: 'Customer'},
+    {path: '/dashboard/customerOrder', label: 'Customer Order'},
+    {path: '/dashboard/customerOrderItem', label: 'Customer Order Item'},
+    {path: '/dashboard/employee', label: 'Employee'},
+    {path: '/dashboard/inventory', label: 'Inventory'},
+    {path: '/dashboard/invoice', label: 'Invoice'},
+    {path: '/dashboard/invoiceItem', label: 'Invoice Item'},
+    {path: '/dashboard/order', label: 'Order'},
+    {path: '/dashboard/orderItem', label: 'Order Item'},
+    {path: '/dashboard/producer', label: 'Producer'},
+    {path: '/dashboard/region', label: 'Region'},
+    {path: '/dashboard/store', label: 'Store'},
+    {path: '/dashboard/supplier', label: 'Supplier'}
+  ]
+  
 }
