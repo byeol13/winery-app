60855cb7b011246e3dd1f5ca11447f6378a582d3
ranjahasrama <ranjahasrama@gmail.com>
2024-11-09 16:14:31 +0100

    Shfaq te gjitha te dhenat qe ka tabela country ne country menu/ ne dashboard

--- winery-app/src/app/features/country/components/country-list/country-list.component.ts
+++ winery-app/src/app/features/country/components/country-list/country-list.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatTableModule} from '@angular/material/table';
+import {MatButtonModule} from '@angular/material/button';
+import { Country } from '../../../../shared/models/Country.model';
+import { CountryService } from '../../service/country.service';
 
 @Component({
   selector: 'app-country-list',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule, MatTableModule, MatButtonModule],
   templateUrl: './country-list.component.html',
   styleUrl: './country-list.component.css'
 })
-export class CountryListComponent {
+export class CountryListComponent implements OnInit{
+
+  countries: Country[] = [];
+  displayedColumns: string[] = ['id', 'country_name'];
+
+  constructor(private countryService: CountryService){}
+  
+  ngOnInit(): void {
+    this.loadAllCountries();
+  }
+
+  loadAllCountries() {
+    this.countryService.getAllCountries().subscribe((res) => {
+      this.countries = res;
+    })
+  }
 
 }
---
+++ winery-app/src/app/features/country/service/country.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Country } from '../../../shared/models/Country.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CountryService {
+
+  private apiUrl = `http://localhost:3000/country`;
+
+  constructor(private http: HttpClient) { }
+
+  getAllCountries(): Observable<Country[]> {
+    return this.http.get<Country[]>(this.apiUrl);
+  }
+}
